63e8b75f9ded514b708614d27adccc0df8bc1030
Yannick Carissan <yannick.carissan@univ-amu.fr>
2019-04-01 20:21:09 +0200

    changement d architecture

--- main.py
+++ main.py
@@ -1,51 +1,18 @@
-#!/usr/bin/env python
-# -*- coding: utf8 -*-
-# Version modifiee de la librairie https://github.com/mxgxw/MFRC522-python
+def bienvenue():
+   return
 
-import RPi.GPIO as GPIO
-import MFRC522
-import signal
+def prologue():
+   #on joue un son de bienvenue
+   bienvenue()
 
-continue_reading = True
+def main():
+   prologue()
+   while true:
+#on attend un évènement
+#on ne fait rien, le lecteur est soit
+#_en train de lire un fichier audio
+#_en train de ne rien faire
+      continue
 
-# Fonction qui arrete la lecture proprement 
-def end_read(signal,frame):
-    global continue_reading
-    print ("Lecture terminée")
-    continue_reading = False
-    GPIO.cleanup()
-
-signal.signal(signal.SIGINT, end_read)
-MIFAREReader = MFRC522.MFRC522()
-
-print ("Passer le tag RFID a lire")
-
-while continue_reading:
-    
-    # Detecter les tags
-    (status,TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
-
-    # Une carte est detectee
-    if status == MIFAREReader.MI_OK:
-        print ("Carte detectee")
-    
-    # Recuperation UID
-    (status,uid) = MIFAREReader.MFRC522_Anticoll()
-
-    if status == MIFAREReader.MI_OK:
-        print ("UID de la carte : "+str(uid[0])+"."+str(uid[1])+"."+str(uid[2])+"."+str(uid[3]))
-    
-        # Clee d authentification par defaut
-        key = [0xFF,0xFF,0xFF,0xFF,0xFF,0xFF]
-        
-        # Selection du tag
-        MIFAREReader.MFRC522_SelectTag(uid)
-
-        # Authentification
-        status = MIFAREReader.MFRC522_Auth(MIFAREReader.PICC_AUTHENT1A, 8, key, uid)
-
-        if status == MIFAREReader.MI_OK:
-            MIFAREReader.MFRC522_Read(8)
-            MIFAREReader.MFRC522_StopCrypto1()
-        else:
-            print ("Erreur d\'Authentification")
+if __name__ == '__main__':
+        main()
